3977b53e4fb2005461efbc53e036600e8cf3e64c
Calvin McLean <calvinmclean@email.arizona.edu>
2019-01-18 14:16:05 -0700

    Remove ansible for nginx setup

--- troposphere/entrypoint.sh
+++ troposphere/entrypoint.sh
@@ -3,19 +3,27 @@
 echo -e $SSH_KEY > /opt/my_key
 chmod 600 /opt/my_key
 echo -e "Host gitlab.cyverse.org\n\tStrictHostKeyChecking no\n\tIdentityFile /opt/my_key" >> ~/.ssh/config
-git clone $SECRETS_REPO /opt/dev/atmosphere-docker-secrets
+git clone $SECRETS_REPO $SECRETS_DIR
 
 source /opt/dev/clank_workspace/clank_env/bin/activate
 cd /opt/dev/clank_workspace/clank
 
-cp /opt/dev/atmosphere-docker-secrets/inis/troposphere.ini /opt/dev/troposphere/variables.ini
+cp $SECRETS_DIR/inis/troposphere.ini /opt/dev/troposphere/variables.ini
 /opt/env/troposphere/bin/python /opt/dev/troposphere/configure
 
-echo "ansible-playbook playbooks/tropo_setup.yml -e @/opt/dev/atmosphere-docker-secrets/clank_vars.yml"
-ansible-playbook playbooks/tropo_setup.yml -e @/opt/dev/atmosphere-docker-secrets/clank_vars.yml
+echo "ansible-playbook playbooks/tropo_setup.yml -e @$SECRETS_DIR/clank_vars.yml"
+ansible-playbook playbooks/tropo_setup.yml -e @$SECRETS_DIR/clank_vars.yml
 
 # Configure and run nginx
-ansible-playbook playbooks/configure_nginx.yml -e @/opt/dev/atmosphere-docker-secrets/clank_vars.yml
+. $SECRETS_DIR/tropo_vars.env
+echo "cp $TLS_BYO_PRIVKEY_DIR /etc/ssl/private/localhost.key"
+cp $TLS_BYO_PRIVKEY_DIR /etc/ssl/private/localhost.key
+echo "cp $TLS_BYO_CERT_DIR /etc/ssl/certs/localhost.crt"
+cp $TLS_BYO_CERT_DIR /etc/ssl/certs/localhost.crt
+echo "cp $TLS_BYO_CACHAIN_DIR /etc/ssl/certs/localhost.cachain.crt"
+cp $TLS_BYO_CACHAIN_DIR /etc/ssl/certs/localhost.cachain.crt
+echo "cat /etc/ssl/certs/localhost.crt /etc/ssl/certs/localhost.cachain.crt > /etc/ssl/certs/localhost.fullchain.crt"
+cat /etc/ssl/certs/localhost.crt /etc/ssl/certs/localhost.cachain.crt > /etc/ssl/certs/localhost.fullchain.crt
 nginx
 
 # Wait for DB to be active
